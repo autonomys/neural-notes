1864910c941cc028367847d7532fd560d8630432
Jeremy Frank <jeremy@subspace.network>
2023-05-17 15:06:10 -0600

    feat: use gpt4 for creating release notes from PR summaries

--- src/release-notes.ts
+++ src/release-notes.ts
@@ -1,16 +1,30 @@
 import { OpenAI } from 'langchain/llms/openai';
 import { ExtractedPRData, generatePRInputData } from './fetch-prs';
 
-const summarizePrs = async (prData: ExtractedPRData, model: OpenAI) => {
+const openAIApiKey = localStorage.getItem('openai-api-key') || '';
+
+const gpt3 = new OpenAI({
+    modelName: 'gpt-3.5-turbo',
+    openAIApiKey: openAIApiKey,
+    temperature: 0.1,
+});
+
+const gpt4 = new OpenAI({
+    modelName: 'gpt-4',
+    openAIApiKey: openAIApiKey,
+    temperature: 0.1,
+});
+
+const summarizePrs = async (prData: ExtractedPRData) => {
     const pr = JSON.stringify(prData);
     console.log(pr);
-    const response = await model.call(
+    const response = await gpt3.call(
         `summarize the following PR for purposes of generating relase notes from an array of summaries:\n\n${JSON.stringify(
             pr
         )}`
     );
     console.log(response);
-
+    console.log(`response len: ${response.length} pr len: ${pr.length}`);
     return response;
 };
 
@@ -19,24 +33,15 @@ export const generateReleaseNotes = async (
     startDate: Date,
     endDate: Date
 ) => {
-    const openAIApiKey = localStorage.getItem('openai-api-key') || '';
-    const model = new OpenAI({
-        modelName: 'gpt-3.5-turbo',
-        openAIApiKey: openAIApiKey,
-        temperature: 0.1,
-    });
-
     const prs = await generatePRInputData(repoUrl, startDate, endDate);
-    const summaries = await Promise.all(
-        prs.map((pr) => summarizePrs(pr, model))
-    );
+    const summaries = await Promise.all(prs.map((pr) => summarizePrs(pr)));
 
     const prompt = `Please generate properly formatted in markdown release notes from the following pull request summaries:\n\n${JSON.stringify(
         summaries
-    )}\n\n Categorize by new features, improvements, and bug fixes in the release. Do not include an "other" category. Do not give specific information about PR labels, commits or comments.`;
+    )}\n\n Categorize by new features, improvements, and bug fixes in the release. Do not include an "other" category. Do not give specific information about individual PRs including titles, labels, commits or comments.`;
     console.log(prompt);
 
-    const response = await model.call(prompt);
+    const response = await gpt4.call(prompt);
     console.log(response);
     return response;
 };
